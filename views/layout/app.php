be795a5fcbd65a11707a140105123cac439320dc
Sergio Fernandoy carrasco <macdesergio@MacBook-Air-de-Sergio.local>
2022-05-12 12:50:21 -0400

    header html finalizado

--- views/layout/app.php
+++ views/layout/app.php
@@ -1,7 +1,18 @@
-<?php
-
-include_once('../models/conexion.php');
-echo "kek";
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pruebitaslocas</title>
+    <link rel="stylesheet" href="../../public/assets/Css/style.css">
+                             
+    
+</head>
+<body class="bgFull">
+    <?php 
+    include("header.php");
+    ?>
+    
+</body>
+</html>
--- views/layout/header.php
+++ views/layout/header.php
@@ -1,6 +1,11 @@
-<header>
-    <p> Obtenga hasta un 25% de reembolso en el primer pedido : GET25OFF </p>
-    <p>Llamanos Lorem, ipsum.</p>
+
+<header class="bgFull">
+    <div>
+        <p> Obtenga hasta un 25% de reembolso en el primer pedido : GET25OFF </p>
+    </div>
+    <div>
+        <p>Llamanos Lorem, ipsum.</p>
+    </div>
     <div><!--Selectores-->
         <div>
             <select name="Idioma" id="">
@@ -22,15 +27,28 @@
     <div><!--Logo-->
         <img src="../../public/assets/img/logoMeta.jpg" alt="LogoPrincipal" width="90px" height="60px">
     </div>
-    <div><!--Busqueda-->
-        <input type="text" value="Búsqueda en catálogo">
+    <div>
+        <div><!--Busqueda-->
+            <input type="text" value="Búsqueda en catálogo">
+        </div>
+        <div><!--lupa busqueda-->
+            <button>
+                <img src="../../public/assets/img/lupita.png" alt="" width="20px">
+            </button>
+        </div>
+    </div>
+    <div>
+        <div>
+            <button>
+            <img src="../../public/assets/img/personita.png" alt="" width="20px">
+            </button>
+        </div>
+        <div>
+            <button>
+            <img src="../../public/assets/img/carrito.png" alt="" width="20px">
+            </button>
+        </div>
     </div>
 
-    <div>
-        <button><!---->
-            <img src="../../public/assets/img/lupita.png" alt="" width="10px">
-        </button>
-    </div>
-    
 
 </header>
