462b126fe1e97b93b097066b4a4cab4b2c608898
Sergio Fernandoy carrasco <macdesergio@MacBook-Air-de-Sergio.local>
2022-05-12 11:43:17 -0400

    avance footer html

---
+++ views/layout/header.php
@@ -0,0 +1,36 @@
+<header>
+    <p> Obtenga hasta un 25% de reembolso en el primer pedido : GET25OFF </p>
+    <p>Llamanos Lorem, ipsum.</p>
+    <div><!--Selectores-->
+        <div>
+            <select name="Idioma" id="">
+                <option value="">Español</option>
+                <option value="">English</option>
+                <option value="">Frances</option>
+                <option value="">Italiano</option>
+            </select>
+        </div>
+        <div>
+            <select name="Moneda" id="">
+                <option value="">CLP</option>
+                <option value="">EUR</option>
+                <option value="">USD</option>
+            </select>
+        </div>
+    </div>
+
+    <div><!--Logo-->
+        <img src="../../public/assets/img/logoMeta.jpg" alt="LogoPrincipal" width="90px" height="60px">
+    </div>
+    <div><!--Busqueda-->
+        <input type="text" value="Búsqueda en catálogo">
+    </div>
+
+    <div>
+        <button><!---->
+            <img src="../../public/assets/img/lupita.png" alt="" width="10px">
+        </button>
+    </div>
+    
+
+</header>
