d11d310d1f4277db2db4fc73b63de97e6b2419d2
Victor Valdivia <victorvaldivia@MacBook-Air-de-Victor.local>
2022-05-12 13:19:22 -0400

    css solucionado

--- public/index.php
+++ public/index.php
@@ -1,19 +1,5 @@
-<?php 
-    include('../models/conexion.php');
-    include('../models/producto.php');
+<?php
+
+include_once("../views/layout/app.php");
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        $producto = new Producto(1,"Tostadas",1500,"Tostadas de frutas","Frutas",10);
-         
-    ?>
-</body>
-</html>
--- views/layout/app.php
+++ views/layout/app.php
@@ -5,14 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pruebitaslocas</title>
-    <link rel="stylesheet" href="../../public/assets/Css/style.css">
-                             
-    
+    <link rel="stylesheet" href="assets/Css/style.css">
 </head>
 <body class="bgFull">
-    <?php 
-    include("header.php");
-    ?>
+   
+   <?php include("header.php"); ?>
     
 </body>
 </html>
